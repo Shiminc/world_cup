cc9afa89ca7582bc81cbb991f75720a554709917
Shiminc <shiminc@gmail.com>
2026-07-07 15:27:19 +0100

    added more

--- javascripts/analysisData.js
+++ javascripts/analysisData.js
@@ -16,7 +16,26 @@ analysis_data = [
             "At the same time, European big teams Spain, Germany, England except France also tend to be playing in their own leagues and less diverse.",
             "It seems, therefore, if you already have a strong domestic league, it is common to have your representatives from there and it might not be a disadvantageous. But if your domestic league is not that strong yet, it would probably be advantageous to have players playing in other countries, as in the case of the better performing Asian representatives Japan and Australia. Is this mirroring general migration patterns, you would want to go to a country strong in the field you are pursuing if your country is not yet there? Possibly in the future, you would be able to bring back the experience?",
             "You could explore the above observations by clicking sort with most mixed teams on the legend panel." 
-        ]}
+        ]},
+        {"variable_name":"birth_place", 
+            "analysis":[
+                "Top Birth Place",
+                "So much media has been focused on the fact that France 'produced' (pun intended) the most number of players in the World Cup. A closer look at those players also revealed some human migration trend in relataion to colonial past. Beside France national team itself, Algeria, Haiti, DR Congo, Senegal all have at least 10 players born in France, followed by Ivory Coast and Tunisia with 8 and 7 players respectively. All these countries used to be the colonised by France, it is likely that players' ancestors might have migrated to France either voluntary or forced slavery. Nonetheless, all these countries except DR Congo also at least nine players born locally",
+                "Similarly, another top 'producer' is Netherlands, which to some extent produced two teams in the world cup, Netherlands national team itself and Curacao, which used to be colonised by the Dutch. Nonetheless, Curacao does have a player born there, Chong and with the surname, you know, he is of Chinese descent. What a wonderful mix of culture!",
+                "You could explore the above observations by selecting France and Netherlands on the legend.",
+                "Most Mixed team",
+                "Most interesting thing to observe is that Bosnia and Herzegovina, Qatar, Tunisia, Morrocco, Cape Verde seem to have most mixed of players born in different countries. Given its history, quite a few players representing Bosnia and Herzegovina players come from countries breaking up from Yugoslavia.", 
+                "The unexplained surprise to me is that of Qatar which have players born in quite a diverse place including Egypt, Portugal, Brazil, Senegal, Ghana, Sudan, Tunisia, France, Belgium and Algeria.",
+                "There are eight national teams which hail all players born locally, and another 10 have all but one player born locally. It worth poiting out that Japan and South Korea, which was stereotypically viewed as homogeneous society, each recruits one player not born locally, that is Suzuki and Castrop.",
+                "Nonetheless, in total, there are 28 national teams with 80% players born locally. You could easily see it in the 'Whether players were born local' tab.",
+                "Therefore, it seems that birth place and national identity is still strongly tie together, compared to where you work or live.",
+                "You could explore the above observations by clicking sort with most mixed teams on the legend panel.",
+            ]
+        },
+        {"variable_name":"play_local",    
+        "analysis":[
+
+        ]},  
 ]
 
 //     {"variable_name":"birth_place", "label":"Where players were born",
@@ -27,10 +46,7 @@ analysis_data = [
 //         "sort":"Sort with teams with most players in Big 5 on top",
 //         "sort_index":"proportion_big_five",   
 //         "data":league_data},
-//     {"variable_name":"play_local", "label":"Whether players play in local leagues",
-//         "sort":"Sort with teams with most players playing in local leagues",
-//         "sort_index":"proportion_play_local",   
-//         "data":play_local_data},    
+  
 //     {"variable_name":"born_local", "label":"Whether players were born local",
 //         "sort":"Sort with teams with most players born locally",
 //         "sort_index":"proportion_born_local",   
